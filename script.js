2bf4c7eb590336f0a7206e5a833be0c328e890ad
FyloFeiaFotita <111823643+FyloFeiaFotita@users.noreply.github.com>
2022-08-22 17:58:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function GoBackToOfficialWebsite() {
+    window.open("https://www.fylofeiafotita.com");
+}
+
+function GoToInfoGoogleDoc() {
+    window.open("https://docs.google.com/document/d/1e0zRh0xnJEnecE0ikQtKpR37NSNkqAim0c68xG0TCqo/edit?usp=sharing");
+}
+
+function GoToTwitter() {
+    window.open("https://twitter.com/fylofeiafotita/");
+}
+function GoToInstagram() {
+    window.open("https://www.instagram.com/fylofeiafotita/");
+}
